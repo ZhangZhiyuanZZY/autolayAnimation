e277b544f176759230ca03b1ee6ef6dc1cb6b7aa
章芝源 <610925874@qq.com>
2015-12-24 12:54:04 +0800

    使用约束做动画, 的方法

---
+++ animation/animation/ViewController.m
@@ -0,0 +1,40 @@
+//
+//  ViewController.m
+//  animation
+//
+//  Created by 章芝源 on 15/12/24.
+//  Copyright © 2015年 ZZY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *topCos;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    self.topCos.constant += 100;
+    
+    [UIView animateWithDuration:2.0 animations:^{
+         [self.view layoutIfNeeded];
+    }];
+   
+    
+   
+}
+
+@end
